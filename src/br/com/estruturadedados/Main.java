e05d72090221856d21d34343d08880c63b4890ec
Marlon <marlon673@hotmail.com>
2021-12-28 15:34:24 -0300

    Adicionando Fila

--- src/br/com/estruturadedados/Main.java
+++ src/br/com/estruturadedados/Main.java
@@ -1,7 +1,16 @@
 package br.com.estruturadedados;
 
+import br.com.estruturadedados.fila.Fila;
+
 public class Main {
     public static void main(String[] args){
-
+        //fila
+        Fila f = new Fila(10);
+        for(int i = 0; i < 10;i++){
+            f.insere(i);
+        }
+        while(!f.isEmpty()){
+            f.remove();
+        }
     }
 }
---
+++ src/br/com/estruturadedados/fila/Fila.java
@@ -0,0 +1,53 @@
+package br.com.estruturadedados.fila;
+
+public class Fila {
+    private int inicio;
+    private int fim;
+    private int memo[];
+    private int tamMax;
+
+    public Fila(int tamMax){
+        this.tamMax = tamMax;
+        this.memo   = new int [tamMax];
+        this.inicio = 0;
+        this.fim    = 0;
+    }
+
+    public void insere(int elemento){
+        if(!isFull()){
+            this.memo[fim] = elemento;
+            this.fim ++;
+        }else{
+            System.out.println("erro: fila cheia");
+        }
+    }
+
+    public int remove(){
+        int elemento = 0;
+        if(!isEmpty()){
+            elemento = this.memo[inicio];
+            this.inicio++;
+        }else{
+            System.out.println("erro: fila vazia");
+        }
+        return elemento;
+    }
+
+    public boolean isFull(){
+        if(this.inicio == this.fim)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isEmpty(){
+        if(this.inicio == this.fim)
+            return true;
+        else
+            return false;
+    }
+
+
+
+
+}//class
