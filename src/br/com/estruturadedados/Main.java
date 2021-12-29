99e12aeda2b6f5f9b0085e6cce21b74d73a8d9dd
Marlon <marlon673@hotmail.com>
2021-12-29 11:25:57 -0300

    removendo pacote fila

--- src/br/com/estruturadedados/Main.java
+++ src/br/com/estruturadedados/Main.java
@@ -1,17 +1,7 @@
 package br.com.estruturadedados;
 
-import br.com.estruturadedados.fila.Fila;
-
 public class Main {
     public static void main(String[] args){
-        //fila
-        Fila f = new Fila(10);
-        for(int i = 0; i < 10;i++){
-            f.insere(i);
-        }
-        System.out.println("*Fila");
-        while(!f.isEmpty()){
-            System.out.println(f.remove());
-        }
+
     }
 }
